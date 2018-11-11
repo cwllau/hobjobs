b28ebd8c9d0c6c9c5bf492e882114d81ae4be765
gfilangeri <gfilange@calpoly.edu>
2018-11-10 17:26:57 -0800

    single listing and all listings

--- app/src/main/java/com/example/priyanka/mapsdemo/JobListings.java
+++ app/src/main/java/com/example/priyanka/mapsdemo/JobListings.java
@@ -1,7 +1,10 @@
 package com.example.priyanka.mapsdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JobListings extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class JobListings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_listings);
+
+        Button btn1 = (Button)findViewById(R.id.job1);
+        Button btn2 = (Button)findViewById(R.id.job2);
+        Button btn3 = (Button)findViewById(R.id.job3);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(JobListings.this, SingleJobListing.class));
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(JobListings.this, SingleJobListing.class));
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(JobListings.this, SingleJobListing.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/priyanka/mapsdemo/SingleJobListing.java
+++ app/src/main/java/com/example/priyanka/mapsdemo/SingleJobListing.java
@@ -1,7 +1,10 @@
 package com.example.priyanka.mapsdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SingleJobListing extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class SingleJobListing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_job_listing);
+
+        Button backbtn = (Button)findViewById(R.id.backButton);
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SingleJobListing.this, JobListings.class));
+            }
+        });
     }
 }
